5c8841409eaec78275d1e3fe565cd137b2c0ce70
AF <af.arthurfaria@gmail.com>
2025-02-12 22:17:40 -0300

    modularizacao no codigo

--- nivelamento/Questao03.java
+++ nivelamento/Questao03.java
@@ -1,43 +1,54 @@
 import java.util.Scanner;
 
 public class Questao03 {
+  static Scanner teclado;
+
+  public static double lerNumero(String mensagem){
+    double numero;
+    System.out.print(mensagem);
+    numero = teclado.nextDouble();
+
+    return numero;
+  }
+
+  public static void lerNotas(double[] atividades){
+    for(int i = 0; i < atividades.length; i++){
+      atividades[i] = lerNumero("\tNota " + (i + 1) + " :  " );
+    }
+  }
+
+  public static double calcularMedia(double[] atividades, double peso){
+    double media, soma = 0;
+    for(int i = 0; i < atividades.length; i++){
+      soma += atividades[i];
+    }
+    media = soma / atividades.length;
+    return media * peso;
+  }
+
   public static void main(String[] args) {
-    Scanner teclado = new Scanner(System.in);
+    teclado = new Scanner(System.in);
 
-    float[] exercicios = new float[4];
-    float[] provas = new float[2];
-    float trabalho;
+    double[] exercicios = new double[4];
+    double[] provas = new double[2];
+    double notaTrabalho;
 
-    float somaExercicios = 0;
-    float somaProvas = 0;
-    float notaFinal;
+    double notaFinal;
 
-    System.out.println("Digite as notas dos exercícios (0 a 100): ");
-    for (int i = 0; i < exercicios.length; i++) {
-      System.out.print(String.format("Exercício %02d: ", i + 1));
-      exercicios[i] = teclado.nextFloat();
-      somaExercicios += exercicios[i];
-    }
+    System.out.println("\nDigite as notas dos exercícios (0 a 100): ");
+    lerNotas(exercicios);
 
-    System.out.println("\nDigite as notas das provas (0 a 100): ");
-    for (int i = 0; i < provas.length; i++) {
-      System.out.print(String.format("Prova %02d: ", i + 1));
-      provas[i] = teclado.nextFloat();
-      somaProvas += provas[i];
-    }
+    System.out.println("Digite as notas das provas (0 a 100): ");
+    lerNotas(provas);
 
-    System.out.print("\nDigite a nota do trabalho prático (0 a 20): ");
-    trabalho = teclado.nextFloat();
+    notaTrabalho = lerNumero("Digite a nota do trabalho prático (0 a 20): ");
+
+    double mediaExercicios = calcularMedia(exercicios, 0.2);
+    double mediaProvas = calcularMedia(provas, 0.6);
+
+    notaFinal = mediaExercicios + mediaProvas + notaTrabalho;
+    System.out.printf("\nSua nota final é: %.2f\n", notaFinal);
 
     teclado.close();
-
-    float mediaExercicios = somaExercicios / exercicios.length;
-    float mediaProvas = somaProvas / provas.length;
-
-    float trabalhoAjustado = (trabalho / 20) * 100;
-
-    notaFinal = (mediaExercicios * 0.2f) + (mediaProvas * 0.6f) + (trabalhoAjustado * 0.2f);
-
-    System.out.println(String.format("\nSua nota final é: %.2f", notaFinal));
   }
 }
